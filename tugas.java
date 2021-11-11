91c8e6edb074d80575a61197b6e7e226f39cf270
= <=>
2021-11-11 22:28:53 +0700

    first commit

---
+++ tugas.java
@@ -0,0 +1,7 @@
+public class tugas {
+    public static void main(String[] args){
+        System.out.println("Nama  : Muhammad Navis Abdillah");
+        System.out.println("Kelas : TI-3A");
+        System.out.println("NIM   : 1941720147");
+    }
+}
